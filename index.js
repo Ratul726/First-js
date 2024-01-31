6420e5600cff1d241a1db612403b112d640560ea
Ratul726 <ratulx726492@gmail.com>
2024-01-31 10:25:21 -0800

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var name = "Adar killa banadar with ban";
-console.log(name);
-var coop = parseFloat(22.01);
-var cpoi = parseInt(22.10)
-console.log(coop ,cpoi)
+// var name = "Adar killa banadar with ban";
+// console.log(name);
+// var coop = parseFloat(22.01);
+// var cpoi = parseInt(22.10);
+// console.log(coop ,cpoi);
+const array = ['apple', 'banana', 'coconut', 'grape', 'mango'];
+for(let i = array.length-1 ; i>= 0; i--){
+    console.log(array[i])
+}
